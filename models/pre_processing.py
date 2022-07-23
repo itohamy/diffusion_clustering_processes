47ed4548c8d2b5d92070f6aadf92b5ef826fbc2a
iritchelly <tohamy@post.bgu.ac.il>
2022-07-23 13:59:11 +0300

    l

--- models/pre_processing.py
+++ models/pre_processing.py
@@ -12,11 +12,11 @@ class PreProcess(nn.Module):
         super(PreProcess, self).__init__()
 
 
-    def forward(self, X, Z):
+    def forward(self, X, Z_t):
         '''
             X (input): input data, shape: [B, N, d] where N is the same in each entry of the batch and d is a small dimension of the data.
-            Z (input): perturbed ground-truth continous representation of the discrete labels of X, shape: [B, N, K]
-            Q (output): combnation of Z and X, shape: [B, N, K, d+1]
+            Z_t (input): perturbed ground-truth continous representation of the discrete labels of X, shape: [B, N, K, q]
+            Q (output): combnation of Z and X, shape: [B, N, K, d+q]
         '''
 
 
